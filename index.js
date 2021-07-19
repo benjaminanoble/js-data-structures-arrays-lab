203040a68664611bbb93fe6027293a073a546d75
benjaminnoble <benjaminanoble@icloud.com>
2021-07-19 21:35:49 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendDriver() {
+    drivers.push('Ralph');
+}
+
+function destructivelyPrependDriver() {
+    drivers.unshift('Bob');
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+}
+
+function appendDriver() {
+    return [...drivers.slice(), 'Broom']
+}
+
+function prependDriver() {
+    return ['Arnold',...drivers.slice()]
+}
+
+function removeLastDriver() {
+    return [...drivers.slice(0,2)]
+}
+
+function removeFirstDriver() {
+    return [...drivers.slice(-2)]
+}
